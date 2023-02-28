026fb1f6f04322c17d6a36678aba45af44a2e36c
Damian <damiansuaz@gmail.com>
2023-02-28 10:29:34 -0500

    Remove ONE line of unnecessary code :p

--- commands/public/coinflip.js
+++ commands/public/coinflip.js
@@ -4,10 +4,8 @@ module.exports = {
 	detailed: 'For when you need to decide something.',
 	admin: false,
 	execute(message, args, globals) {
-		let result = '';
+		let result = 'Tails!';
 		if (Math.floor(Math.random() * 2) == 0) result = 'Heads!';
-		else result = 'Tails!';
-
 		return message.channel.send(result);
 	},
 };
